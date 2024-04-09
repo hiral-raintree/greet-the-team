612419a0c485be4f55e3e96b6f26dfeeecb23feb
hiralg <hiralg@cybage.com>
2024-04-09 21:39:31 +0530

    Added merged API & Event Bridge

--- bin/demo-stack.ts
+++ bin/demo-stack.ts
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import { LambdaStack } from '../lib/lambda-stack';
-// import { AppSyncStack } from '../lib/appsync-stack';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { RTProviderServiceCognitoUserPool } from '../lib/cognito-stack';
 import { RTProviderAppSyncAPI } from '../lib/provider-appsync-stack';
 import { RDSStack } from '../lib/rds-stack';
+import { MergedAPIStack } from '../lib/merged-api-stack';
+
+import { LambdaStack } from '../lib/lambda-stack'; 
+import { EventBus } from 'aws-cdk-lib/aws-events'
+import { Rule } from 'aws-cdk-lib/aws-events';
+import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
 
 const app = new cdk.App();
-
-// create a new Lambda
-// new LambdaStack(app, 'lambdaStack');
-// new AppSyncStack(app, 'appSyncStack');
 
 export class RTProviderServiceStack extends cdk.Stack {
   constructor(
@@ -66,5 +66,56 @@ export class RTProviderServiceStack extends cdk.Stack {
   }
 }
 
+export class RTMergedAPIStack extends cdk.Stack {
+  constructor(
+    scope: Construct,
+    id: string,
+  ) {
+    super(scope, id);
+    new MergedAPIStack(this, 'RTMergedAPI', { name: 'RTMergedAPI' });
+  }
+}
+
+// Instantiate the Merged API Stack Class
+// new RTMergedAPIStack(app, 'RT-Provider-Service-Merged-API')
+
+
+export class RTEventBridgeStack extends cdk.Stack {
+  constructor(
+    scope: Construct,
+    id: string,
+  ) {
+    super(scope, id);
+
+    const bus = new EventBus(this,'RTEventBus',{
+      eventBusName: "RTEventBus"
+    })
+
+    const rule = new Rule(this, "RTEventRule", {
+      description: "RTEventRuleDescription",
+      eventPattern: {
+        source: ["aws.lambda"]
+      },
+      eventBus: bus
+    });
+
+    const basicLambda = new LambdaStack(this, 'RT-basic-lambda-stack', { eventBusARN: bus.eventBusArn })
+
+    rule.addTarget(new LambdaFunction(basicLambda.lambdaFunction));
+
+    new cdk.CfnOutput(this, 'EventBusARN', {
+      value: bus.eventBusArn,
+    });
+
+    new cdk.CfnOutput(this, 'EventRuleARN', {
+      value: rule.ruleArn,
+    });
+  }
+}
+
+// Instantiate the Merged API Stack Class
+new RTEventBridgeStack(app, 'RT-Provider-Service-EventBridge')
+
+
 // instantiate the main stack class  
-new RTProviderServiceStack(app, 'RT-Provider-Service');
+// new RTProviderServiceStack(app, 'RT-Provider-Service');
--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,19 +1,38 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 import { Construct } from 'constructs';
 
-export class LambdaStack extends cdk.Stack {
+interface lambdaStackProps {
+  eventBusARN: string;
+}
+
+export class LambdaStack extends Construct {
   public readonly lambdaFunction: lambda.Function;
 
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: Construct, id: string, props: lambdaStackProps) {
+    super(scope, id);
 
-    this.lambdaFunction = new lambda.Function(this, "greet_the_team", {
-      functionName: "greet_the_team",
+    this.lambdaFunction = new lambda.Function(this, "RT-greet_the_team", {
+      functionName: "RT-greet_the_team",
       runtime: lambda.Runtime.PYTHON_3_8,
       code: lambda.Code.fromAsset("src/lambda/basic_lambda"),
       handler: "greet_the_team.index_handler",
     });
+
+    this.lambdaFunction.role?.attachInlinePolicy(
+      new iam.Policy(this, 'eventBusPolicy', {
+        document: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              effect: iam.Effect.ALLOW,
+              actions: ['events:PutEvents'],
+              resources: [props.eventBusARN],
+            }),
+          ],
+        }),
+      })
+    )
   }
 }
